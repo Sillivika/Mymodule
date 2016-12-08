eed55ed5ad7b85186cc4ceb53811ffdce3e1e988
Jori Laine <jori_laine@hotmail.com>
2016-12-08 21:34:15 +0200

    More settings for desktop

--- puppet/modules/mymodule/manifests/init.pp
+++ puppet/modules/mymodule/manifests/init.pp
@@ -10,6 +10,11 @@ class mymodule {
 	service {"lightdm":
 		ensure => "running",
 		enable => "true",
+		require => File['/home/cube/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml'],
 		provider => "systemd",
+	}
+	file { "/home/cube/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml":
+		content => template("mymodule/xfce4-desktop.xml"),
+		require => File['/usr/share/xfce4/backdrops/xubuntu-wallpaper.png'],
 	}
 }
